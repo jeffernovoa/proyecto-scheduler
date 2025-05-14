a0caed99049c70d5a9f7a7c77de65aed619213b0
jeffernovoa <jefferng35@gmail.com>
2025-05-14 09:55:46 +0200

    v3

---
+++ src/fcfs_scheduler.py
@@ -0,0 +1,15 @@
+from scheduler import Scheduler
+
+class FCFSScheduler(Scheduler):
+    def planificar(self, procesos):
+        tiempo_actual = 0
+        gantt = []
+
+        for p in procesos:
+            p.tiempo_inicio = tiempo_actual
+            tiempo_final = tiempo_actual + p.duracion
+            p.tiempo_fin = tiempo_final
+            gantt.append((p.pid, p.tiempo_inicio, p.tiempo_fin))
+            tiempo_actual = tiempo_final
+
+        return gantt
---
+++ src/rr_scheduler.py
@@ -0,0 +1,31 @@
+from scheduler import Scheduler
+from collections import deque
+
+class RoundRobinScheduler(Scheduler):
+    def __init__(self, quantum):
+        self.quantum = quantum
+
+    def planificar(self, procesos):
+        tiempo_actual = 0
+        gantt = []
+        cola = deque(procesos)
+
+        for p in cola:
+            p.tiempo_restante = p.duracion
+
+        while cola:
+            p = cola.popleft()
+            if p.tiempo_inicio is None:
+                p.tiempo_inicio = tiempo_actual
+
+            tiempo_ejec = min(self.quantum, p.tiempo_restante)
+            p.tiempo_restante -= tiempo_ejec
+            tiempo_actual += tiempo_ejec
+            gantt.append((p.pid, tiempo_actual - tiempo_ejec, tiempo_actual))
+
+            if p.tiempo_restante > 0:
+                cola.append(p)
+            else:
+                p.tiempo_fin = tiempo_actual
+
+        return gantt
